f046e7d25c501baf549647b262e75e09cdc57fc7
Jamal Shahverdiyev <jamal.shahverdiev@gmail.com>
2019-09-03 22:59:16 +0500

    Update DEB script

--- vagrant-ansible-centos-debian/scripts/debGLscript.sh
+++ vagrant-ansible-centos-debian/scripts/debGLscript.sh
@@ -21,6 +21,10 @@ if [ $(hostname -f) = dnsmasq -o $(hostname -f) = controller ]
 then
     apt install -y python-pip
     pip install hostsman dnspython
+    apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    apt update && apt-get install -y docker-ce docker-ce-cli containerd.io
 fi
 
 cat <<EOF >> /etc/hosts
